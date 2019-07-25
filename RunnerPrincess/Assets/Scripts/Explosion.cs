04e57352cf673c769651e711767c3796c13db0fd
KidCozy <pencube1214@naver.com>
2019-07-25 20:37:44 +0900

    Character Resource and boom

--- RunnerPrincess/Assets/Scripts/Explosion.cs
+++ RunnerPrincess/Assets/Scripts/Explosion.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class Explosion : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Animator animator;
+    float exitTime = 0.8f;
+
+    IEnumerator CheckAnimationState()
     {
-        
+
+        while (!animator.GetCurrentAnimatorStateInfo(0)
+        .IsName("boom"))
+        {
+
+            Debug.Log("yes");
+            //전환 중일 때 실행되는 부분
+            yield return null;
+        }
+
+        while (animator.GetCurrentAnimatorStateInfo(0)
+        .normalizedTime < exitTime)
+        {
+            Debug.Log("noani");
+            //애니메이션 재생 중 실행되는 부분
+            yield return null;
+        }
+
+        //애니메이션 완료 후 실행되는 부분
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        StartCoroutine(CheckAnimationState());
     }
 }
