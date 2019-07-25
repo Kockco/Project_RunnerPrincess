c88b042d0f8dd9e8dd7af52a1a28040886c03c4e
KidCozy <pencube1214@naver.com>
2019-07-25 20:57:48 +0900

    explosion

--- RunnerPrincess/Assets/Scripts/Explosion.cs
+++ RunnerPrincess/Assets/Scripts/Explosion.cs
@@ -7,36 +7,36 @@ public class Explosion : MonoBehaviour
     Animator animator;
     float exitTime = 0.8f;
 
-    IEnumerator CheckAnimationState()
-    {
+    //IEnumerator CheckAnimationState()
+    //{
 
-        while (!animator.GetCurrentAnimatorStateInfo(0)
-        .IsName("boom"))
-        {
+    //    while (!animator.GetCurrentAnimatorStateInfo(0)
+    //    .IsName("boom"))
+    //    {
 
-            Debug.Log("yes");
-            //전환 중일 때 실행되는 부분
-            yield return null;
-        }
+    //        Debug.Log("yes");
+    //        //전환 중일 때 실행되는 부분
+    //        yield return null;
+    //    }
 
-        while (animator.GetCurrentAnimatorStateInfo(0)
-        .normalizedTime < exitTime)
-        {
-            Debug.Log("noani");
-            //애니메이션 재생 중 실행되는 부분
-            yield return null;
-        }
+    //    while (animator.GetCurrentAnimatorStateInfo(0)
+    //    .normalizedTime < exitTime)
+    //    {
+    //        Debug.Log("noani");
+    //        //애니메이션 재생 중 실행되는 부분
+    //        yield return null;
+    //    }
 
-        //애니메이션 완료 후 실행되는 부분
+    //    //애니메이션 완료 후 실행되는 부분
 
-    }
+    //}
 
-    private void Start()
-    {
-        animator = GetComponent<Animator>();
-    }
-    private void Update()
-    {
-        StartCoroutine(CheckAnimationState());
-    }
+    //private void Start()
+    //{
+    //    animator = GetComponent<Animator>();
+    //}
+    //private void Update()
+    //{
+    //    StartCoroutine(CheckAnimationState());
+    //}
 }
