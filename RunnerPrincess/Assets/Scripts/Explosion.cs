5b8ebfee1edd235796a91a5dcd6d1d6410b3524d
KidCozy <pencube1214@naver.com>
2019-07-25 21:44:34 +0900

    커밋

--- RunnerPrincess/Assets/Scripts/Explosion.cs
+++ RunnerPrincess/Assets/Scripts/Explosion.cs
@@ -5,38 +5,33 @@ using UnityEngine;
 public class Explosion : MonoBehaviour
 {
     Animator animator;
-    float exitTime = 0.8f;
+    BoxCollider2D myBox;
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        myBox = GetComponent<BoxCollider2D>();
+    }
+    private void Update()
+    {
+        if (!animator.GetCurrentAnimatorStateInfo(0).IsName("boom"))
+        {
+            Debug.Log("충돌불가");
+            myBox.enabled = false;
+        }
+        else if (animator.GetCurrentAnimatorStateInfo(0).IsName("boom"))
+        {
+            Debug.Log("충돌가능");
+            myBox.enabled = true;
+        }
+    }
 
-    //IEnumerator CheckAnimationState()
-    //{
 
-    //    while (!animator.GetCurrentAnimatorStateInfo(0)
-    //    .IsName("boom"))
-    //    {
 
-    //        Debug.Log("yes");
-    //        //전환 중일 때 실행되는 부분
-    //        yield return null;
-    //    }
-
-    //    while (animator.GetCurrentAnimatorStateInfo(0)
-    //    .normalizedTime < exitTime)
-    //    {
-    //        Debug.Log("noani");
-    //        //애니메이션 재생 중 실행되는 부분
-    //        yield return null;
-    //    }
-
-    //    //애니메이션 완료 후 실행되는 부분
-
-    //}
-
-    //private void Start()
-    //{
-    //    animator = GetComponent<Animator>();
-    //}
-    //private void Update()
-    //{
-    //    StartCoroutine(CheckAnimationState());
-    //}
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.transform.name == "Character")
+        {
+            Debug.Log("die");
+        }
+    }
 }
