fec5a2ab5be65c81b9d32643bd07836292692c42
jiyong <jiyong7989@gmail.com>
2019-07-25 19:11:51 +0900

    Character Script

---
+++ RunnerPrincess/Assets/0_Script/PlayerManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    Rigidbody2D _rigid;
+
+    // Player Speed
+    public int _speed;
+
+    // Horizontal , Vertical
+    float h;
+    float v;
+
+    private void Awake()
+    {
+        _rigid = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        PlayerMove();
+        PlayerRun();
+    }
+
+    private void PlayerMove()
+    {
+        h = Input.GetAxisRaw("Horizontal") * _speed * Time.deltaTime;
+        v = Input.GetAxisRaw("Vertical") * _speed * Time.deltaTime;
+        transform.Translate(new Vector3(h, v, 0));
+    }
+
+    private void PlayerRun()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            _speed *= 2;
+            if(Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                _speed = 3;
+            }
+        }
+        
+    }
+
+}
